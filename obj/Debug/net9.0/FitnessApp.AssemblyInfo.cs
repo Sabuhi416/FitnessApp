86666f8e5cb74908434eec05eca314d92a697fd4
Sabuhi416 <sebuhiabbasov1514@gmail.com>
2025-05-14 08:47:22 +0400

    Fixed image link

--- obj/Debug/net9.0/FitnessApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/FitnessApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitnessApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac86eec2e73ef13480740feba4cbf685dde29251")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitnessApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitnessApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
